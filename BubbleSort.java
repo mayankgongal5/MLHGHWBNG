f0d391cc656b14b8e28b5141c37d05b70fac1c31
Mayank Ashok Gongal <111949604+mayankgongal5@users.noreply.github.com>
2024-01-06 09:55:07 +0530

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,36 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original Array:");
+        printArray(array);
+
+        // Perform bubble sort
+        bubbleSort(array);
+
+        System.out.println("\nSorted Array:");
+        printArray(array);
+    }
+
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element found is greater than the next element
+                if (arr[j] > arr[j + 1]) {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
